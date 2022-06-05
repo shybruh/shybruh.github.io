981de3f16c7c0bc4beecf8d03aed33949fc1c179
1889 <82598703+shybruh@users.noreply.github.com>
2022-06-05 21:37:27 +0800

    new
    q

---
+++ assets/js/parallax.js
@@ -0,0 +1,28 @@
+// Made by ertugrulozcan
+// https://github.com/ertugrulozcan/
+// https://gist.github.com/ertugrulozcan/f663d68764dc9c09e169
+
+$(document).mousemove(function(event) {
+    cx = Math.ceil($(window).width() / 1.8);
+    cy = Math.ceil($(window).height() / 1.8);
+    dx = event.pageX - cx;
+    dy = event.pageY - cy;
+
+    tiltx = (dy / cy);
+    tilty = - (dx / cx);
+    radius = Math.sqrt(Math.pow(tiltx,2) + Math.pow(tilty,2));
+    degree = (radius * 20);
+    
+    $('#pres').css('-webkit-transform','rotate3d(' + -tiltx + ', ' + -tilty + ', 0, ' + degree + 'deg)');
+    $('#pres').css('transform','rotate3d(' + -tiltx + ', ' + -tilty + ', 0, ' + degree + 'deg)');
+
+});
+
+function changeAnimState() {
+    $(".eye-left").clone(true).insertBefore(".eye-left");
+    $(".eye-left").last().remove();
+	$(".eye-right").clone(true).insertBefore(".eye-right");
+    $(".eye-right").last().remove();
+}
+
+setInterval(changeAnimState, 3000);
