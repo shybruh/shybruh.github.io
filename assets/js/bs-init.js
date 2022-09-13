cd7046815fefad1b1b0dafba2eb09181e5f9c2ed
1889 <82598703+shybruh@users.noreply.github.com>
2022-09-13 23:37:09 +0530

    new site

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,5 +1,8 @@
-$(document).ready(function(){
-	$('[data-bss-hover-animate]')
-		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
-		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bss-hover-animate')) });
-});
+document.addEventListener('DOMContentLoaded', function() {
+
+	var hoverAnimationTriggerList = [].slice.call(document.querySelectorAll('[data-bss-hover-animate]'));
+	var hoverAnimationList = hoverAnimationTriggerList.forEach(function (hoverAnimationEl) {
+		hoverAnimationEl.addEventListener('mouseenter', function(e){ e.target.classList.add('animated', e.target.dataset.bssHoverAnimate) });
+		hoverAnimationEl.addEventListener('mouseleave', function(e){ e.target.classList.remove('animated', e.target.dataset.bssHoverAnimate) });
+	});
+}, false);
