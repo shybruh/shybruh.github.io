ba560726e87adf8ca6c3394b456e923eb286ad07
1889 <82598703+shybruh@users.noreply.github.com>
2023-08-30 14:51:48 +0500

    Update full shit

---
+++ js/audio.js
@@ -0,0 +1,15 @@
+var audio = document.getElementById('audio');
+
+var init = false;
+audio.volume = 0.085;
+setInterval(() => {
+    if (!init) {
+        audio.play()
+        .then(() => {
+            start();
+            init = true;
+        })
+        .catch(err => {})
+    }
+}, 100);
+
---
+++ js/textch.js
@@ -0,0 +1,35 @@
+let message = {
+    messages: [
+        '"The cycle ends here. We must be better." - Kratos ', 
+        '"A man chooses. A slave obeys." - Andrew Ryan ', 
+        '"Good men mean well. We just dont always end up doing well." - Isaac Clarke ', 
+        '"Its not the end of the world, but you can see it from here." - Colt ', 
+        '"Im not a hero. Never was, never will be." - Arthur Morgan ',
+        'You cant break a pot. You can only break the idea of a pot." - Yagami', 
+        '"Im here to make my mark. You know what they say, no risk, no reward." - Mirage ', 
+        '"Remember, no Russian." - Makarov ', 
+        '"The numbers, Mason! What do they mean?" - Reznov ', 
+        '"We get dirty, and the world stays clean." - Shepherd ', 
+        '"Bravo Six, Going Dark" - Captain Price '
+    ],
+    up: true,
+    index: 0,
+    count: 0,
+    start: function() {
+        message.interval = setInterval(() => {
+            if (message.count == message.messages[message.index].length) {
+                message.up = false
+                clearInterval(message.interval)
+                setTimeout(message.start, 750)
+            } else if (message.count == 1) {
+                message.up = true
+                message.index = message.index == message.messages.length -1 ? 0 : ++message.index
+            }
+
+            message.count = message.up ? ++message.count : --message.count
+            document.getElementById('index_message').textContent = message.messages[message.index].slice(0, message.count)
+        }, 75);
+    }
+}
+
+message.start()
