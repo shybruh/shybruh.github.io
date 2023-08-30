8069ec8a7f8561e74d0f880a0277d1e886960f7d
1889 <82598703+shybruh@users.noreply.github.com>
2023-08-30 14:58:41 +0500

    smol updates

--- js/audio.js
+++ js/audio.js
@@ -5,11 +5,10 @@ audio.volume = 0.085;
 setInterval(() => {
     if (!init) {
         audio.play()
-        .then(() => {
-            start();
-            init = true;
-        })
-        .catch(err => {})
+            .then(() => {
+                start();
+                init = true;
+            })
+            .catch(err => { })
     }
 }, 100);
-
--- js/textch.js
+++ js/textch.js
@@ -1,21 +1,21 @@
 let message = {
     messages: [
-        '"The cycle ends here. We must be better." - Kratos ', 
-        '"A man chooses. A slave obeys." - Andrew Ryan ', 
-        '"Good men mean well. We just dont always end up doing well." - Isaac Clarke ', 
-        '"Its not the end of the world, but you can see it from here." - Colt ', 
+        '"The cycle ends here. We must be better." - Kratos ',
+        '"A man chooses. A slave obeys." - Andrew Ryan ',
+        '"Good men mean well. We just dont always end up doing well." - Isaac Clarke ',
+        '"Its not the end of the world, but you can see it from here." - Colt ',
         '"Im not a hero. Never was, never will be." - Arthur Morgan ',
-        'You cant break a pot. You can only break the idea of a pot." - Yagami', 
-        '"Im here to make my mark. You know what they say, no risk, no reward." - Mirage ', 
-        '"Remember, no Russian." - Makarov ', 
-        '"The numbers, Mason! What do they mean?" - Reznov ', 
-        '"We get dirty, and the world stays clean." - Shepherd ', 
+        'You cant break a pot. You can only break the idea of a pot." - Yagami',
+        '"Im here to make my mark. You know what they say, no risk, no reward." - Mirage ',
+        '"Remember, no Russian." - Makarov ',
+        '"The numbers, Mason! What do they mean?" - Reznov ',
+        '"We get dirty, and the world stays clean." - Shepherd ',
         '"Bravo Six, Going Dark" - Captain Price '
     ],
     up: true,
     index: 0,
     count: 0,
-    start: function() {
+    start: function () {
         message.interval = setInterval(() => {
             if (message.count == message.messages[message.index].length) {
                 message.up = false
@@ -23,7 +23,7 @@ let message = {
                 setTimeout(message.start, 750)
             } else if (message.count == 1) {
                 message.up = true
-                message.index = message.index == message.messages.length -1 ? 0 : ++message.index
+                message.index = message.index == message.messages.length - 1 ? 0 : ++message.index
             }
 
             message.count = message.up ? ++message.count : --message.count
@@ -31,5 +31,4 @@ let message = {
         }, 75);
     }
 }
-
 message.start()
